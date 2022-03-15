29f0d4ccdfd5331f2138a253cf5eaeb4708d36b8
ahmet <akocoglu@gmail.com>
2022-03-15 13:58:39 +0300

    react install

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,43 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 const axios = require('axios');
 
 function App() {
-  const name = "Net";
-  const surname = "Adım";
-  const api = axios.get('https://betametre.com/api')
-      .then(function (response) {
-        // handle success
-        console.log(response);
-        return response;
-      })
-      .catch(function (error) {
-        // handle error
-          console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/blog')
+            .then(function (response) {
+                // handle success
+                console.log(response.data);
+                setUser(response.data);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+    }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {name} {surname}
-            {api.statusCode}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    {user.name} {user.surname}
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
