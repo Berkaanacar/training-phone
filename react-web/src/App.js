60dd4ca0d660c69a077e95157a1f667f85209983
ahmet <akocoglu@gmail.com>
2022-03-15 12:23:56 +0300

    react install

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+const axios = require('axios');
 
 function App() {
+  const name = "Net";
+  const surname = "Adım";
+  const api = axios.get('https://betametre.com/api')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        return response;
+      })
+      .catch(function (error) {
+        // handle error
+          console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name} {surname}
+            {api.statusCode}
         </p>
         <a
           className="App-link"
